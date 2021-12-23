9ab0820281cc7cd0c35f9ebcdda78af858060d30
sohyeonAn <ansh4393@gmail.com>
2021-12-23 22:42:04 +0900

    🎨 next/images 를 위한 domain 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['thumbs.vivino.com', 'images.vivino.com', 'www.totalwine.com'],
+  },
 }
